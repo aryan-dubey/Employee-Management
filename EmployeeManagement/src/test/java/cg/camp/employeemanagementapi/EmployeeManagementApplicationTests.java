b20dc6eabfead34c2c3aead970fa907ddad22841
Anish AKarapu <akarapuanish@gmail.com>
2022-03-24 10:51:05 +0530

    first commit.

---
+++ EmployeeManagement/src/test/java/cg/camp/employeemanagementapi/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package cg.camp.employeemanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
