8873b0625d1a4657bc29f9c136ba8c6503c16df2
aryan-dubey <itsaryanborn2rock@gmail.com>
2022-03-31 00:12:42 +0530

    Custom Exception Compliance Added

---
+++ employeemanagementapi/src/main/java/cg/camp/employeemanagementapi/exception/CustomResponseEntityExceptionHandlerComp.java
@@ -0,0 +1,24 @@
+package cg.camp.employeemanagementapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+
+@ControllerAdvice
+@RestController
+public class CustomResponseEntityExceptionHandler1 extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler
+	public final ResponseEntity<Object> handleComplianceIdException(ComplianceIdException ex, WebRequest request){
+		ComplianceIdExceptionResponse exceptionResponse = new ComplianceIdExceptionResponse(ex.getMessage());
+		return new ResponseEntity<Object>(exceptionResponse,HttpStatus.BAD_REQUEST);
+	}
+	
+	
+}
